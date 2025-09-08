faaa7c9f523b1abd54baac01a6823c841de7eb73
admin <admin@admin.kr>
2025-09-08 11:37:19 +0900

    for p

--- server/server.js
+++ server/server.js
@@ -4,6 +4,16 @@ const studentRoutes = require("./routes/studentRoutes");
 const connection = require("./db");
 
 const app = express();
+
+const os = require("os");
+
+const network = os.networkInterfaces();
+const ip =
+  (network["enp7s0"] && network["enp7s0"][0].address) ||
+  (network["이더넷"] && network["이더넷"][1].address) ||
+  (network["Wi-Fi"] && network["Wi-Fi"][1].address) ||
+  "127.0.0.1";
+
 const PORT = 3000;
 
 app.use(cors());
@@ -11,6 +21,9 @@ app.use(express.json());
 
 // API 라우터 연결
 app.use("/api/students", studentRoutes);
+app.use("/api/ip", (req, resp) => {
+  resp.json({ ip: ip });
+});
 
 // 서버 시작
 app.listen(PORT, () => {
